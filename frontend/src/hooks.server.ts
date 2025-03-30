6224d7070067014c2a3997e431b869fb32dbdd03
FranekDev <franek.w2000@gmail.com>
2025-03-30 19:30:02 +0200

    feat(frontend): redirect unauthorized user

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,11 +1,15 @@
-import type { Handle } from "@sveltejs/kit";
+import { redirect, type Handle } from "@sveltejs/kit";
 import type { User } from "./lib/types/user";
 
 export const handle: Handle = async ({ event, resolve }) => {
+    const publicRoutes = ["/", "/success"];
     const jwt = event.cookies.get("jwt_token");
     
     if (!jwt) {
         event.locals.user = null;
+        if (!publicRoutes.includes(event.url.pathname)) {
+            return redirect(302, "/");
+        }
     } else {
         event.locals.user = { jwt_token: jwt } as User;
     }
